b61be3ce4aea62fa62ae5438d8b54dfe0a3265b7
Brian Ruff <briananthonyruff@gmail.com>
2022-12-31 15:17:56 -0500

    updates

--- index.js
+++ index.js
@@ -23,6 +23,20 @@ let endDate = null;
 let count = 0;
 let randomTickRate = getRandomTickRate();
 
+// D3 initialization
+// set the dimensions and margins of the graph
+// var margin = {top: 10, right: 30, bottom: 30, left: 50},
+// width = 460 - margin.left - margin.right,
+// height = 400 - margin.top - margin.bottom;
+// append the svg object to the body of the page
+// var svg = d3.select("#my_dataviz")
+// .append("svg")
+// .attr("width", width + margin.left + margin.right)
+// .attr("height", height + margin.top + margin.bottom)
+// .append("g")
+// .attr("transform",
+//     "translate(" + margin.left + "," + margin.top + ")");
+
 function getRandomColor() {
     return colors[Math.floor(Math.random() * colors.length)];
 }
@@ -34,8 +48,6 @@ function getRandomTickRate() {
 function changeBoardColor() {
     startDate = 0;
     endDate = 0;
-
-    console.log('randomTickRate: ', randomTickRate)
     
     changeBoardColorIntervalID = setInterval(() => {
         score = 0;
@@ -49,7 +61,7 @@ function changeBoardColor() {
 
             genreatedColors.push(randomColor);
 
-            if (genreatedColors.length === 1 && randomColor === 'green') {
+            if (genreatedColors.length === 1 && isBoardGreen()) {
                 randomColor = getRandomColor();
             }
 
@@ -66,10 +78,10 @@ function changeBoardColor() {
             genreatedColors.push(randomColor);
             
             board.style.backgroundColor = randomColor;
+            
             startDate = +new Date();
-            console.log('count: ', count)
 
-            if (randomColor === 'green') {
+            if (isBoardGreen()) {
                 ding().play();
             }
         }
@@ -85,14 +97,18 @@ function isInteractionTooEarly() {
 }
 
 
+function isBoardGreen() {
+    return board.style.backgroundColor === 'green';
+}
+
 function setUpGame(event) {
     event.stopImmediatePropagation();
     gameStarted = true;
     changeBoardColor();
-    hideResetButton();
-    startButton.style.display = 'none';
-    headerDiv.style.display = 'none';
-    directionsDiv.style.display = 'none';
+    toggleResetButton(false);
+    toggleStartButton(false);
+    toggleHeader(false);
+    toggleDirections(false);
 }
 
 function clickedGameBoard(event) {
@@ -114,19 +130,20 @@ function clickedGameBoard(event) {
             
             evalDiv.textContent = `Congrats! You clicked on the green color. Your reaction time is ${Math.round(score)}! ms`;
             
-            scoreList.push(score);
+            scoreList.push([{date: moment().format('YYYY-MM-DD')}, {value: score}]);
 
             // append score to score list if list is less than 5 items
             if (scoreList.length < 5) {
-                showResetButton('Try again');
+                toggleResetButton(true);
                 const scoreListItem = document.createElement('li');
                 scoreListItem.textContent = `${scoreList.length}: ${Math.round(score)} ms`;
                 scoreListDiv.appendChild(scoreListItem);
             }
+            console.log(scoreList)
 
             // if score list is 5 items, calculate average score and display it
             if (scoreList.length === 5) {
-                showResetButton('Reset');
+                toggleResetButton(true, 'Reset');
                 const averageScore = scoreList.reduce((a, b) => a + b) / scoreList.length;
                 const scoreListItem = document.createElement('li');
                 scoreListItem.textContent = `${scoreList.length}: ${Math.round(score)} ms`;
@@ -140,11 +157,13 @@ function clickedGameBoard(event) {
             }
         } else {
             // if not green color, reset game, show reset button with Try again text, and display evaluation text with error message
-            showResetButton('Try again');
+            toggleResetButton(true);
             hasNotClickedGreen = true;
             evalDiv.textContent = `You did not click green. Please try again. However, your reaction time from the last color to the one you clicked was ${Math.round(score)} ms!`;
             evalDiv.style.color = 'white';
         }
+
+        setupD3LineChart(scoreList);
     }
 }
 
@@ -170,7 +189,7 @@ function resetGame(event) {
     // if start button is hidden, show it
     if (startButton.style.display === 'none') {
         startButton.style.display = 'block';
-        hideResetButton();
+        toggleResetButton(false);
     }
 
     // after resetting game state
@@ -189,23 +208,36 @@ function resetGame(event) {
     board.style.backgroundColor = 'lightblue';
 }
 
-startButton.addEventListener('mousedown', setUpGame);
-startButton.addEventListener('touchstart', setUpGame);
-
-resetButton.addEventListener('mousedown', resetGame);
-resetButton.addEventListener('touchstart', resetGame);
-
-board.addEventListener('mousedown', clickedGameBoard);
-board.addEventListener('touchstart', clickedGameBoard);
-
-function showResetButton(text = '') {
-    resetButton.style.display = 'block';
-
-    if (text) resetButton.textContent = text;
+function toggleStartButton(shouldShow) {
+    if (shouldShow) {
+        startButton.style.display = 'block';
+    } else {
+        startButton.style.display = 'none';
+    }
 }
 
-function hideResetButton(text = '') {
-    resetButton.style.display = 'none';
+function toggleHeader(shouldShow) {
+    if (shouldShow) {
+        headerDiv.style.display = 'block';
+    } else {
+        headerDiv.style.display = 'none';
+    }
+}
+
+function toggleDirections(shouldShow) {
+    if (shouldShow) {
+        directionsDiv.style.display = 'block';
+    } else {
+        directionsDiv.style.display = 'none';
+    }
+}
+
+function toggleResetButton(shouldShow, text = 'Try again') {
+    if (shouldShow) {
+        resetButton.style.display = 'block';
+    } else {
+        resetButton.style.display = 'none';
+    }
 
     if (text) resetButton.textContent = text;
 }
@@ -216,3 +248,67 @@ function ding() {
     audioPlayer.setAttribute('autoplay', 'autoplay');
     return audioPlayer;
 }
+
+startButton.addEventListener('mousedown', setUpGame);
+startButton.addEventListener('touchstart', setUpGame);
+
+resetButton.addEventListener('mousedown', resetGame);
+resetButton.addEventListener('touchstart', resetGame);
+
+board.addEventListener('mousedown', clickedGameBoard);
+board.addEventListener('touchstart', clickedGameBoard);
+
+// function setupD3LineChart(data = []) {
+//     // Keep only the 90 first rows
+//     // data = data.filter(function(d,i){ return i<90})
+
+//     // Add X axis --> it is a date format
+//     var x = d3.scaleTime()
+//     .domain(d3.extent(data, function(d) { return d.date; }))
+//     .range([ 0, width ]);
+//     svg.append("g")
+//     .attr("transform", "translate(0," + (height+5) + ")")
+//     .call(d3.axisBottom(x).ticks(5).tickSizeOuter(0));
+
+//     // Add Y axis
+//     var y = d3.scaleLinear()
+//     .domain( d3.extent(data, function(d) { return +d.value; }) )
+//     .range([ height, 0 ]);
+//     svg.append("g")
+//     .attr("transform", "translate(-5,0)")
+//     .call(d3.axisLeft(y).tickSizeOuter(0));
+
+//     // Add the area
+//     svg.append("path")
+//     .datum(data)
+//     .attr("fill", "#69b3a2")
+//     .attr("fill-opacity", .3)
+//     .attr("stroke", "none")
+//     .attr("d", d3.area()
+//         .x(function(d) { return x(d.date) })
+//         .y0( height )
+//         .y1(function(d) { return y(d.value) })
+//         )
+
+//     // Add the line
+//     svg.append("path")
+//     .datum(data)
+//     .attr("fill", "none")
+//     .attr("stroke", "#69b3a2")
+//     .attr("stroke-width", 4)
+//     .attr("d", d3.line()
+//         .x(function(d) { return x(d.date) })
+//         .y(function(d) { return y(d.value) })
+//         )
+
+//     // Add the line
+//     svg.selectAll("myCircles")
+//     .data(data)
+//     .enter()
+//     .append("circle")
+//         .attr("fill", "red")
+//         .attr("stroke", "none")
+//         .attr("cx", function(d) { return x(d.date) })
+//         .attr("cy", function(d) { return y(d.value) })
+//         .attr("r", 3)
+// }
