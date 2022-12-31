46ec7e25d2aa05e61dd207d3d86fbcc5a0cda8df
Brian Ruff <briananthonyruff@gmail.com>
2022-12-31 02:10:49 -0500

    increase accuracy of score, and add average time after 5 tries

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const board = document.querySelector('#board');
 const evalDiv = document.querySelector('#evaluation');
 const headerDiv = document.querySelector('#header');
 const directionsDiv = document.querySelector('#directions');
+const scoreListDiv = document.querySelector('#scoreList');
+const averageScoreDiv = document.querySelector('#averageScore');
 
 
-const colors = ['red', 'green', 'blue', 'orange', 'purple'];
+const colors = ['red', 'green', 'blue', 'orange'];
 
 let score = 0;
 let gameStarted = false;
@@ -15,20 +17,26 @@ let hasNotClickedGreen = false;
 let scoreIntervalID = null;
 let changeBoardColorIntervalID = null;
 let gameBoardID = null;
-let randomTickRate = null;
 let tickRate = 10;
+let scoreList = [];
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
 
 function changeBoardColor() {
     score = 0;
+
     if (hasClickedGreen || hasNotClickedGreen) {
         clearTimeout(scoreIntervalID);
         clearTimeout(changeBoardColorIntervalID);
-        cancelAnimationFrame(gameBoardID);
     }
+
     if (gameStarted) {
-        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        let randomColor = getRandomColor();
         board.style.backgroundColor = randomColor;
-        randomTickRate = Math.floor(Math.random() * 1000) + 1000;
+        const randomTickRate = Math.floor(Math.random() * 1000) + 1000;
+        if (changeBoardColorIntervalID) clearTimeout(changeBoardColorIntervalID);
         changeBoardColorIntervalID = setTimeout(changeBoardColor, randomTickRate);
     }
 }
@@ -51,29 +59,51 @@ function setUpGame(event) {
     score = 0;
     hasClickedGreen = false;
     hasNotClickedGreen = false;
-    startButton.style.display = 'none';
-    resetButton.style.display = 'none';
-    headerDiv.style.display = 'none';
-    directionsDiv.style.display = 'none';
     gameStarted = true;
     gameLoop();
-    increaseScore();
     changeBoardColor();
+    increaseScore();
+    hideResetButton();
+    startButton.style.display = 'none';
+    headerDiv.style.display = 'none';
+    directionsDiv.style.display = 'none';
 }
 
-function startGameBoard(event) {
+function clickedGameBoard(event) {
     event.stopImmediatePropagation();
     if (gameStarted) {
-        resetButton.style.display = 'block';
+        showResetButton();
         gameStarted = false;
         const boardColor = board.style.backgroundColor;
         if (boardColor === 'green') {
             hasClickedGreen = true;
             evalDiv.textContent = `Congrats! You clicked on the green color. Your reaction time is ${Math.round(score)}! ms`;
-            evalDiv.style.color = 'lightred';
+            evalDiv.textContent = `Congrats! You clicked on the green color. Your reaction time is ${Math.round(score)}!`;
+            scoreList.push(score);
+
+            // append score to score list if list is less than 5 items
+            if (scoreList.length < 5) {
+                showResetButton();
+                const scoreListItem = document.createElement('li');
+                scoreListItem.textContent = `${scoreList.length}: ${score} ms`;
+                scoreListDiv.appendChild(scoreListItem);
+            }
+
+            // if score list is 5 items, calculate average score and display it
+            if (scoreList.length === 5) {
+                const averageScore = scoreList.reduce((a, b) => a + b) / scoreList.length;
+                const scoreListItem = document.createElement('li');
+                scoreListItem.textContent = `${scoreList.length}: ${score} ms`;
+                scoreListDiv.appendChild(scoreListItem);
+                averageScoreDiv.textContent = `Your average reaction time over the past five tries is ${Math.round(averageScore)} ms!`;
+            }
+
+            if (scoreList.length > 5) {
+                resetGame(event);
+            }
         } else {
             hasNotClickedGreen = true;
-        clearTimeout(gameBoardID);
+            clearTimeout(gameBoardID);
         evalDiv.textContent = `You did not click green. Please try again. However, your reaction time from the last color to the one you clicked was ${Math.round(score)} ms!`;
             evalDiv.style.color = 'white';
         }
@@ -82,15 +112,36 @@ function startGameBoard(event) {
 
 function resetGame(event) {
     event.stopImmediatePropagation();
+
+    if (gameStarted && scoreList.length < 5) {
+        scoreList = [];
+        while (scoreListDiv.firstChild) {
+            scoreListDiv.removeChild(scoreListDiv.firstChild);
+        }
+        averageScoreDiv.textContent = '';
+    }
+
+    // if start button is hidden, show it
+    if (startButton.style.display === 'none') {
+        startButton.style.display = 'block';
+        hideResetButton();
+    }
+
     gameStarted = false;
     evalDiv.textContent = '';
     headerDiv.style.display = 'block';
     directionsDiv.style.display = 'block';
-    resetButton.style.display = 'none';
-    startButton.style.display = 'block';
     board.style.backgroundColor = 'lightblue';
 }
 
 startButton.addEventListener('mousedown', setUpGame);
 resetButton.addEventListener('mousedown', resetGame);
-board.addEventListener('mousedown', startGameBoard);
+board.addEventListener('mousedown', clickedGameBoard);
+
+function showResetButton() {
+    resetButton.style.display = 'block';
+}
+
+function hideResetButton() {
+    resetButton.style.display = 'none';
+}
